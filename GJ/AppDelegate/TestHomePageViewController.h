59fa6066602047e5da23a22e52c0141e805bcc20
chenxiaoqiang <312933275@qq.com>
2015-10-10 15:45:17 +0800

    init pro

---
+++ GJ/AppDelegate/TestHomePageViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestHomePageViewController.h
+//  GJ
+//
+//  Created by chenxiaoqiang on 15/10/10.
+//  Copyright © 2015年 ZKL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestHomePageViewController : UIViewController
+
+@end
---
+++ GJ/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GJ
+//
+//  Created by E269 on 14-11-28.
+//  Copyright (c) 2014年 ZKL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
